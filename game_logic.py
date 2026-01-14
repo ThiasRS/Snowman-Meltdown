368aeba2f1fbcfd243b6c0f907ac717d26ad66ed
Matthias Dellinger <thiasgaming@outlook.de>
2026-01-14 11:47:55 +0100

    splitted the snowman.py file into game_logic and ascii_art, added a new function check_win, enhanced the game_logic

--- game_logic.py
+++ game_logic.py
@@ -1,34 +1,5 @@
 import random
-
-# List of melting snowman stages
-STAGES = [
-     # Stage 0: Full snowman
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
-     ( : ) 
-     """,
-     # Stage 1: Bottom part starts melting
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
-     """,
-     # Stage 2: Only the head remains
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     """,
-     # Stage 3: Snowman completely melted
-     """
-      ___  
-     /___\\ 
-     """
- ]
+from ascii_art import STAGES
 
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
@@ -39,34 +10,57 @@ def get_random_word():
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
 
-def display_game_states(mistakes, secret_word, guessed_letter):
+def display_game_states(mistakes, secret_word, guessed_letters):
     """Displays the game states."""
     print(STAGES[mistakes])
     print("Word:", end=" ")
     for letter in secret_word:
-        if guessed_letter.lower() == letter:
+        if letter.lower() in guessed_letters:
             print(letter, end=" ")
         else:
             print("_", end=" ")
     print()
 
 
+def check_win(secret_word, guessed_letters):
+    """checks if all letters are guessed correctly."""
+    for letter in secret_word:
+        # Even if one letter of the word is not
+        # in our list, we have not won yet.
+        if letter not in guessed_letters:
+            return False
+
+    # When our loop without a 'return False' runs,
+    # all letters are correctly -> win!
+    return True
+
+
 def play_game():
     secret_word = get_random_word()
+    guessed_letters = []
     print("Welcome to Snowman Meltdown!")
     print("Secret word selected: " + secret_word)  # for testing, later remove this line
 
-    guess = "x"
     mistakes = 0
     while True:
-        display_game_states(mistakes, secret_word, guess)
+        display_game_states(mistakes, secret_word, guessed_letters)
+        if check_win(secret_word, guessed_letters):
+            print("Congratulations, you won!")
+            break
+
         guess = input("Guess a letter: ").lower()
-        if guess.lower() not in secret_word:
+
+        if guess not in guessed_letters:
+            guessed_letters.append(guess)
+
+        if guess not in secret_word:
             mistakes += 1
+
         if mistakes == 3:
             print("Game Over! The word was: " + secret_word)
             print(STAGES[mistakes])
             break
+
 
 
 if __name__ == "__main__":
